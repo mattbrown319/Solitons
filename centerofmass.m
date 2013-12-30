8aaf520100d8ad2174440e2d61d292cbd8d22a18
srogstad <vegansraq@gmail.com>
2013-12-30 17:09:07 -0500

    New center of mass function

---
+++ centerofmass.m
@@ -0,0 +1,14 @@
+function x_t = centerofmass( u_xt, xgrid )
+
+index = 1;
+x_t = [];
+numSteps = length(u_xt);
+
+    while index < numSteps
+       x_t_onestep = trapz(xgrid,xgrid'.*(abs(u_xt(:,index)).^2))/trapz(xgrid,(abs(u_xt(:,index))).^2); 
+      x_t = [x_t x_t_onestep];
+      index = index + 1;
+    end
+
+end
+
